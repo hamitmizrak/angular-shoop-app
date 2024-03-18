3334f0bfda2e305f35ce4657ab32a1d49ca2f764
Hamit Mizrak <hamitmizrak@gmail.com>
2024-03-18 14:08:19 +0300

    navbar dynamics data type

--- src/app/blog-navbar/User.ts
+++ src/app/blog-navbar/User.ts
@@ -1,46 +1,65 @@
 // ENUM
-export enum userRolles{
-    admin='ADMIN', 
-    writer="WRITER",
-    user='USER',
-}
+export enum userRolles {
+  admin = 'ADMIN',
+  writer = 'WRITER',
+  user = 'USER',
+} //end enum userRolles
 
 // ABSTRACT
 abstract class Person {
-    // Field (Data Types)
-    id:number;
-    username:string;
-    email:string;
-    password:string;
-    //rolles:userRolles;
-    pictures:string;
-    date:string;
-    link:string;
+  // Field (Data Types)
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+  pictures: string;
+  date: string;
+  link: string;
 
-    // Constructor (Parametreli)
-    constructor(id:number,_username:string, email:string, password:string,pictures:string,date:string,link:string){
-    this.id=id;
-    this.username=_username;
-    this.email=email;
-    this.password=password;
-    this.pictures=pictures;
-    this.date=date;
-    this.link=link;
-    }
+  // Constructor (Parametreli)
+  constructor(
+    id: number,
+    username: string,
+    email: string,
+    password: string,
+    pictures: string,
+    date: string,
+    link: string
+  ) {
+    this.id = id;
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.pictures = pictures;
+    this.date = date;
+    this.link = link;
+  }
 
-    // Method
-    personInformation(){
-        //console.log("Personel Bilgileri: "+this.username+" "+this.email+" "+this.password+" "+this.pictures+" "+this.date+" "+this.link);
-        console.log(`Personel Bilgileri: 
-        ${this.id}  
-        ${this.username}  
-        ${this.email}  
-        ${this.password}  
-        ${this.pictures}
-        ${this.date}
-        ${this.link}
-        `);
-        } //end personInformation
-    } //end Person
+
+} //end abstract Person
 
 // CLASS
+export class User extends Person {
+  // Field (Data Types)
+  rolles: string;
+
+  // Constructor (Parametreli)
+  constructor(
+    id: number,
+    username: string,
+    email: string,
+    password: string,
+    pictures: string,
+    date: string,
+    link: string,
+    rolles: string
+  ) {
+    //abstract'ten gelen
+    super(id, username, email, password, pictures, date, link);
+    this.rolles = rolles;
+  }
+
+  // Method
+}
+
+// INTERFACE
--- src/app/blog-navbar/blog-navbar.component.ts
+++ src/app/blog-navbar/blog-navbar.component.ts
@@ -1,12 +1,153 @@
+// import
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
+// Special Component
+import { User, userRolles } from './User';
+
+// Component
 @Component({
+  // Selector
   selector: 'app-blog-navbar',
-  standalone: true,
-  imports: [],
-  templateUrl: './blog-navbar.component.html',
-  styleUrl: './blog-navbar.component.css'
-})
-export class BlogNavbarComponent {
 
-}
+  // Bu component tek başına çalışabilsin
+  standalone: true,
+
+  // import
+  imports: [CommonModule, RouterLink],
+
+  // Html Template
+  templateUrl: './blog-navbar.component.html',
+
+  // Css Template
+  styleUrl: './blog-navbar.component.css',
+})
+
+// CLASS
+export class BlogNavbarComponent {
+  // Field (Data Types)
+  // User:Array<any>;  ==> Dizi Ekleme
+  // User:any[]=[];  ==> Dizi Ekleme
+  // User:User[]=[]; ==> Dizi Ekleme
+  User: User[] = [
+    // id, _username, email, password, pictures, date, link, rolles
+    {
+      id: 1,
+      username: 'Hamit',
+      email: 'hamitmizrak@gmail.com',
+      password: '123456',
+      pictures: 'admin1.png',
+      date: String(new Date().getFullYear()),
+      link: 'https://admin1/',
+      rolles: userRolles.admin,
+    },
+    {
+      id: 2,
+      username: 'writer1',
+      email: 'writer1@gmail.com',
+      password: '123456',
+      pictures: 'writer.png',
+      date: String(new Date().getFullYear()),
+      link: 'https://writer1/',
+      rolles: userRolles.writer,
+    },
+    {
+      id: 3,
+      username: 'user1',
+      email: 'user1@gmail.com',
+      password: '123456',
+      pictures: 'user1.png',
+      date: String(new Date().getFullYear()),
+      link: 'https://user1/',
+      rolles: userRolles.user,
+    },
+  ];
+
+  // navbar
+  // navBar:{}={}; ==> Object Ekleme
+  navBar: {
+    // Authentication
+    isLogin: boolean;
+
+    // Logo
+    logo: {
+      logoName: string;
+      logoFontAwesome: string;
+      logoPicture: String;
+      logoLink: string;
+      logoTitle: string;
+    };
+
+    // Navbar Link
+    navbarLink:Array<any>;
+
+    // Search
+    search:{
+      name:String;
+      placeholder:string;
+      type:string;
+      style:string;
+    };
+  } = {
+    // Authentication
+    isLogin: false,
+
+    // Logo
+    logo: {
+      logoName: "Logo",
+      logoFontAwesome: "fa fa-solid",
+      logoPicture: "admin.png",
+      logoLink: "/",
+      logoTitle: "Admin Title"
+    },
+
+    // Navbar
+    navbarLink:[
+      {
+        id:1,
+        name:'anasayfa',
+        link:"/index",
+        fontAwesome:"fa-solid fa-house-chimney"
+      },
+      {
+        id:2,
+        name:'my work',
+        link:"#business_id",
+        fontAwesome:"fa-solid fa-user-tie"
+      },
+      {
+        id:3,
+        name:'blog',
+        link:"blog_id",
+        fontAwesome:"fa-solid fa-square-rss"
+      },
+      {
+        id:4,
+        name:'about',
+        link:"#about_id",
+        fontAwesome:"fa-regular fa-address-card"
+      },
+      {
+        id:5,
+        name:'Contact',
+        link:"#contact_id",
+        fontAwesome:"fa-solid fa-map-location-dot"
+      }
+    ],
+
+    search:{
+      name:"search",
+      placeholder:"Arama için",
+      type:"text",
+      style:"btn btn-primary",
+    },
+  };
+
+  // Constructor (Parametreli)
+  constructor(){}
+  // Method
+  darkMode(){
+    alert("Dark Mode");
+  }
+} //end BlogNavbarComponent
